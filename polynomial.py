10492b116a2961ece8ef240d64763f6a2d5585d1
Madeline Wong <mwong20@bu.edu>
2024-01-28 05:08:41 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -4,6 +4,10 @@ class X:
 
     def __repr__(self):
         return "X"
+    
+    def evaluate(self, x): 
+        return x
+        #Evaluate X into the input x value 
 
 class Int:
     def __init__(self, i):
@@ -12,6 +16,10 @@ class Int:
     def __repr__(self):
         return str(self.i)
 
+    def evaluate(self, x):
+        return self.i 
+        #evaluates as i (input x value doesn't matter)
+
 class Add:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -19,6 +27,9 @@ class Add:
 
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+
+    def evaluate(self, x): 
+        return self.p1.evaluate(x) + self.p2.evaluate(x) #the sum of the evaluated values of both terms
 
 class Mul:
     def __init__(self, p1, p2):
@@ -33,6 +44,9 @@ class Mul:
         if isinstance(self.p2, Add) or isinstance(self.p2, Sub): #including Sub as possible second term (int * sub)
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
+    
+    def evaluate(self, x): 
+        return self.p1.evaluate(x) * self.p2.evaluate(x) #the product of the evaluated values of both terms
 
 class Sub:
     def __init__(self, p1, p2):
@@ -41,6 +55,9 @@ class Sub:
 
     def __repr__(self):
         return repr(self.p1) + " - " + repr(self.p2)
+
+    def evaluate(self, x): 
+        return self.p1.evaluate(x) - self.p2.evaluate(x) #the diff of the evaluated values of both terms
     
 class Div:
     def __init__(self, p1, p2):
@@ -56,6 +73,11 @@ class Div:
             return repr(self.p1) + " / ( " + repr(self.p2) + " )" 
         return repr(self.p1) + " / " + repr(self.p2) #both terms are int
 
+    def evaluate(self, x): 
+        return self.p1.evaluate(x) / self.p2.evaluate(x) #the quotient of both evluated terms
+
+
+
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
 print(poly)
 
@@ -68,4 +90,17 @@ print(poly3)
 
 poly4 = Sub( Sub( Int(4), Int(3)), Add( X(), Mul( Int(1), Sub( Mul(X(), X()), Int(1)))))
 print(poly4)
+
+test = Sub( Add(Int(5), Div(Int(4), Int(2))), Mul(Int(1), X()))
+print(test)
+print(test.evaluate(3))
+
+test2 = Sub( X(), Add(Int(5), Int(1)))
+print(test2)
+print(test2.evaluate(2))
+
+test3 = Sub( Add(Int(5), Div(Int(4), Int(4))), Mul(Int(1), X()))
+print(test3)
+print(test3.evaluate(2))
+
 
